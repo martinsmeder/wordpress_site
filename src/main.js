77bf3e2431ac0da78111d2593923f949a9c14521
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-04-23 14:29:19 +0200

    Changed to first person perspective

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import platform from "./platform";
 import player from "./player";
 import targetDummy from "./targetDummy";
@@ -16,24 +15,19 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0, 5, 10);
+
+// Set the camera position to be slightly above the player's head
+camera.position.set(0, 2, 0);
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// // Create controls
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true; // Optional: smooth camera movement
-
 // Add objects to the scene
 scene.add(platform);
 scene.add(player);
 scene.add(targetDummy);
-
-// Define the offset for the camera from the player
-const cameraOffset = new THREE.Vector3(0, 5, 7);
 
 // Define movement speed
 const movementSpeed = 0.1;
@@ -41,8 +35,7 @@ const movementSpeed = 0.1;
 // Define keyboard state
 const keyboard = {};
 
-// Define jump state as an object with a value property (instead of a variable) so
-// that changes made to the object inside the jump function is reflected globally
+// Define jump state as an object with a value property
 let isJumping = { value: false };
 
 // Listen for keyboard events
@@ -60,9 +53,12 @@ document.addEventListener("keyup", (event) => {
   keyboard[event.key] = false;
 });
 
+document.body.addEventListener("mousemove", (event) => {
+  // Logic to update camera direction
+});
+
 // Define an animate function to update the scene and render it
 function animate() {
-  // Request the browser to call animate on the next repaint
   requestAnimationFrame(animate);
 
   // Apply gravity to the player and target dummy
@@ -75,12 +71,11 @@ function animate() {
   if (keyboard["a"]) player.position.x -= movementSpeed;
   if (keyboard["d"]) player.position.x += movementSpeed;
 
-  // Update camera position relative to the player
-  camera.position.copy(player.position).add(cameraOffset);
-  camera.lookAt(player.position);
+  // Update camera position to follow the player's position
+  camera.position.copy(player.position);
 
-  // // Update controls
-  // controls.update();
+  // Update camera look at player
+  camera.lookAt(player.position);
 
   // Render the scene with the camera
   renderer.render(scene, camera);
