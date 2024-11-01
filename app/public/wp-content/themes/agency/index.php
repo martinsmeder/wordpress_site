5ae2dcdbef7afc7315552bd57d99a9e88ec6b224
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-11-01 09:26:53 +0100

    Added code to display content from site editor

--- app/public/wp-content/themes/agency/index.php
+++ app/public/wp-content/themes/agency/index.php
@@ -1,17 +1,24 @@
-<!-- bloginfo: Pulls values from WordPress settings   -->
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?php bloginfo('name'); ?></title>
+    <!-- bloginfo: Pulls values from WordPress settings   -->
+    <title><?php bloginfo('name'); ?></title> 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
     <?php get_header(); ?>
     
     <main>
-        <p>Greetings!</p>
-        <p><?php echo 'This is a glorious custom theme for my ' . get_bloginfo('name') . '!' ?></p>
+        <?php 
+        if (have_posts()) : 
+            while (have_posts()) : 
+                the_post(); 
+                the_content(); // Displays content from WordPress editor
+            endwhile; 
+        else :
+            echo '<p>No content available.</p>'; 
+        endif; 
+        ?>
     </main>
 
     <?php get_footer(); ?>
