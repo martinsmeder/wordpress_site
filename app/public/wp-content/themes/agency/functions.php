2443192be1641ecf5008b5c969f326fb55f7d9a7
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-11-14 18:41:37 +0100

    Enqueued multiple stylesheets

--- app/public/wp-content/themes/agency/functions.php
+++ app/public/wp-content/themes/agency/functions.php
@@ -1,12 +1,11 @@
 <?php
+
 // Register the primary navigation menu
 function register_my_menus() {
     register_nav_menu('header-menu', __('Header Menu'));
 }
 add_action('init', 'register_my_menus');
-?>
 
-<?php
 function display_content() {
     if (have_posts()) : 
         while (have_posts()) : 
@@ -21,9 +20,7 @@ function display_content() {
         echo '<p>No content available.</p>'; 
     endif;
 }
-?>
 
-<?php
 function agency_custom_logo_setup() {
     add_theme_support('custom-logo', array(
         'height'      => 40,  // Expected height
@@ -33,4 +30,14 @@ function agency_custom_logo_setup() {
     ));
 }
 add_action('after_setup_theme', 'agency_custom_logo_setup');
+
+function enqueue_styles() {
+    wp_enqueue_style('index', get_template_directory_uri() . '/styles/index.css', array(), '1.0');
+    wp_enqueue_style('header', get_template_directory_uri() . '/styles/header.css', array(), '1.0');
+    wp_enqueue_style('main', get_template_directory_uri() . '/styles/main.css', array(), '1.0');
+    wp_enqueue_style('footer', get_template_directory_uri() . '/styles/footer.css', array(), '1.0');
+}
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+add_filter('show_admin_bar', '__return_false'); // Hide invisible admin bar
+
 ?>
