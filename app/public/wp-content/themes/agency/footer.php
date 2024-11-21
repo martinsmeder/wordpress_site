54bd3fff50c2a2580a612842befc943c67f5e4df
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-11-21 19:05:33 +0100

    Footer

--- app/public/wp-content/themes/agency/footer.php
+++ app/public/wp-content/themes/agency/footer.php
@@ -1,3 +1,26 @@
 <footer>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    <!-- Note: Should be customizable. -->
+
+    <div class="top-left">
+        <!-- Company icon + company name -->
+        <!-- Fake address  -->
+    </div>
+
+    <div class="top-right">
+        <!-- Home -->
+        <!-- About us -->
+        <!-- Projects -->
+        <!-- Contact us -->
+    </div>
+
+    <div class="bottom">
+        <div class="left">
+            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+        </div>
+        
+        <div class="right">
+            <!-- Social icons  -->
+        </div>
+    </div>
+    
 </footer>
